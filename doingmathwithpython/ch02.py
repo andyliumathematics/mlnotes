e49b7d96694141929736da2ec84c139257a9c29a
liulei <liuleicn1024@gmail.com>
2020-09-15 13:44:27 +0800

    add

---
+++ doingmathwithpython/ch02.py
@@ -0,0 +1,13 @@
+# %%
+import matplotlib.pyplot as plt
+
+# %%
+import numpy as np
+plt.plot(range(30,72),np.linspace(237,78,num=72-30),'g-',label='adsfa')
+plt.xlabel('x-coordinate')
+plt.ylabel('y-coordinate')
+plt.title('Projectile motion of a ball')
+# plt.plot(range(30,72),np.linspace(237,78,num=72-30),'o')
+# %%
+np.linspace(237,78,num=72-30)*np.random.random_sample()
+# %%
