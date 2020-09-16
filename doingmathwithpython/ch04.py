551d3c5ec21384b0824c9ba4c7e14f024db39332
liulei <liuleicn1024@gmail.com>
2020-09-16 11:26:39 +0800

    add

--- doingmathwithpython/ch04.py
+++ doingmathwithpython/ch04.py
@@ -1,6 +1,7 @@
 # %%
 '''
 factor 是分解
+expand 是因式展开
 Symbol 定义变量
 solve 是解方程
 '''
@@ -26,4 +27,18 @@ y = Symbol('y')
 solve(x+38-9*y,dict=True)
 # %%
 plot(x**3-2,(x,-100,200))
+# %%
+# 求积分
+from sympy import *
+x = symbols('x')
+print(integrate(x**2, (x, 1, 2)))
+# %%
+integrate(x**3)
+# %%
+# 求导
+from sympy import Symbol
+x = Symbol('x')
+y = Symbol('y')
+f = x + y
+f.subs({x:10, y: 20})
 # %%
